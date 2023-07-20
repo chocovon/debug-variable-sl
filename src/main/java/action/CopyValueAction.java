07582b7b93ad392932ef7f7f70f70b97a44b6e92
Henady Zakalusky <zakalusky@scnsoft.com>
2023-07-20 23:37:12 +0300

    merged 2 plugins to one.
    to build extractor uncomment
       //rootProject.name = 'debug-variable-extractor'
    in settings.gradle and run dveBuild gradle task.
    
    Extractor uses minimal inner-tool (only 22 Kb) and does not have save/load features.
    JSON pretty formatting is done using jackson library.

--- src/main/java/action/CopyValueAction.java
+++ src/main/java/action/CopyValueAction.java
@@ -13,10 +13,27 @@ import ui.common.SimplePopupHint;
 import ui.dialog.CopyValueDialog;
 import util.debugger.PluginSaveLoader;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.stream.Collectors;
+
 import static config.Config.GEN_CODE_SETTINGS_KEY;
 
 public class CopyValueAction extends XDebuggerTreeActionBase {
     private static final Settings initialSettings = loadSettings();
+    private static final boolean isExtractorPlugin = determineIsExtractorPlugin();
+
+    private static boolean determineIsExtractorPlugin() {
+        try {
+            URL resource = CopyValueAction.class.getResource("/META-INF/plugin.xml");
+            String content = new BufferedReader(new InputStreamReader(resource.openStream()))
+                    .lines().collect(Collectors.joining("\n"));
+            return content.contains("com.github.chocovon.debug-variable-extractor");
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
     private static Settings loadSettings() {
         try {
@@ -32,7 +49,17 @@ public class CopyValueAction extends XDebuggerTreeActionBase {
         try {
             CopyValueDialog popup = new CopyValueDialog(e.getProject(), initialSettings, settings -> {
                 try {
-                    return PluginSaveLoader.genJavaCode(node, settings);
+                    if (isExtractorPlugin && settings.getFormat().equals("json")) {
+                        String jsonString = DebugVarAction.getJsonString(node);
+                        if (settings.isPrettyFormat()) {
+                            ObjectMapper objectMapper = new ObjectMapper();
+                            Object object = objectMapper.readValue(jsonString, Object.class);
+                            jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+                        }
+                        return jsonString;
+                    } else {
+                        return PluginSaveLoader.genJavaCode(node, settings);
+                    }
                 } catch (Exception exception) {
                     return "Error: " + exception.getMessage();
                 }
